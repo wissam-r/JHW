2d08b04fb7797b5034777acd217d42affe4e48d5
jasam-sheja <jasam0jasam0jasam@gmail.com>
2013-11-21 14:10:53 +0200

    السؤال الخامس الطلب الخامس

---
+++ JavaHomework/OOPBasics/src/product/ProductStore.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package product;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+
+/**
+ *
+ * @author DigitalNet
+ */
+public class ProductStore {
+    LinkedList<Product> products;
+    
+    public void addProduct(Product product){
+        products.add(product);
+    }
+    public void addProduct(int id,String name,float price,int quntity){
+        products.add(new Product(id,name,price,quntity));
+    }
+    public Product getProductAtIndex(int i){
+        return products.get(i);
+    }
+    public void sortByName(){
+        Collections.sort(products, Product.getPoductNameCorparator);
+    }
+    public int productCount(){
+        return products.size();
+    }
+}
